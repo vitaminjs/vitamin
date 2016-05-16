115ae6d296ff51e75a50e9dca4d8d6564dd137f6
absolux <a.mouhassine@gmail.com>
2016-05-17 00:40:27 +0100

    remove data class

--- src/model.js
+++ src/model.js
@@ -1,8 +1,6 @@
 
-var _ = require('underscore')
-var Hooks = require('./hooks')
-var Query = require('./query')
-var DataClass = require('./data')
+var _ = require('underscore'),
+    Hooks = require('./hooks')
 
 module.exports = Model
 
@@ -14,85 +12,40 @@ module.exports = Model
 function Model() { this._init.apply(this, arguments) }
 
 /**
- * Default options
- * 
- * @static
+ * Data source driver
  */
-Model.options = {
-  
-  // primary key name
-  'pk': "id",
-  
-  // instance methods
-  'methods': {},
-  
-  // static methods
-  'statics': {},
-  
-  // model attributes definition
-  'attributes': {},
-  
-  // table or collection name
-  'source': undefined,
-  
-}
+Model.prototype.$driver = undefined
 
 /**
- * Model hooks
- * 
- * @static
+ * Model hooks manager
  */
-Model.hooks = new Hooks(Model)
-
-/**
- * Data source driver adapter
- * 
- * @static
- */
-Model.driver = undefined
-
-/**
- * Factory function to instantiate new models without `new` operator
- * 
- * @param {Object} data
- * 
- * @static
- */
-Model.factory = function factory(attributes) {
-  return new this(attributes || {})
-}
+Model.prototype.$hooks = new Hooks()
 
 /**
  * Set up correctly the prototype chain for subclasses
  * 
- * @param {Object} options
- * 
+ * @param {Object} props
+ * @param {Object} statics
  * @static
  */
-Model.extend = function extend(options) {
+Model.extend = function extend(props, statics) {
   var Super = this
-      options = options || {}
   
-  // default constructor simply calls the parent constructor
+  // default constructor simply calls the parent one
   function Model() { Super.apply(this, arguments) }
   
   // set the prototype chain to inherit from `parent`
-  Model.prototype = Object.create(Super.prototype, { constructor: { value: Model } })
+  Model.prototype = Object.create(Super.prototype, { 
+    constructor: { value: Model } 
+  })
   
   // add static and instance properties
-  _.extend(Model, Super, options.statics)
-  _.extend(Model.prototype, options.methods)
+  _.extend(Model, Super, statics)
+  _.extend(Model.prototype, props)
   
-  // merge options
-  Model.options = mergeOptions(Super.options, options)
+  // init model hooks
+  Model.prototype.$hooks = Super.prototype.$hooks.clone()
   
-  // init the model hooks
-  Model.hooks = Super.hooks.clone(Model)
-  
-  // define proxies for model attributes
-  setProxies(Model.prototype, options.attributes)
-  
-  // return the final product
   return Model
 }
 
@@ -100,6 +53,8 @@ Model.extend = function extend(options) {
  * Use a plugin
  * 
  * @param {Function|Object} plugin object with `install` method, or simply a function
+ * @return {Model}
+ * @static
  */
 Model.use = function use(plugin) {
   if ( plugin.installed === true ) return this
@@ -123,305 +78,194 @@ Model.use = function use(plugin) {
 }
 
 /**
+ * Factory function to instantiate new models without `new` operator
  * 
- */
-Model.pre = function pre(name, fn, isAsync) {
-  this.hooks.create(name).pre(name, fn, isAsync)
-  return this
-}
-
-/**
- * 
- */
-Model.post = function post(name, fn) {
-  this.hooks.create(name).post(name, fn)
-  return this
-}
-
-/**
- * Get all models from database
- * 
- * @param {Object} where
- * @param {Function} cb
- * 
+ * @param {Object} attrs
+ * @return {Model}
  * @static
  */
-Model.all = function all(cb) {
-  return this.factory().newQuery().fetchAll(cb)
-}
+Model.factory = function factory(attrs) { return new this(attrs) }
 
 /**
- * Find a model by its primary key
- * 
- * @param {Mixed} id
- * @param {Function} cb
- * 
- * @static
- */
-Model.find = function find(id, cb) {
-  return this.where(this.options.pk, id).fetch(cb)
-}
-
-/**
- * 
- * @param {String|Object} key attribute name or constraints object
- * @param {Mixed} value attribute value
- * 
- * @return Query
- */
-Model.where = function where(key, value) {
-  return this.factory().newQuery().where(key, value)
-}
-
-/**
- * Create and save a new model
- * 
- * @param {Object} data object
- * @param {Function} callback
- * 
- * @static
- */
-Model.create = function create(data, cb) {
-  return this.factory(data).save(cb)
-}
-
-/**
- * Get model option by name
+ * Create and add a pre callback
  * 
  * @param {String} name
- * @param {Mixed} defaults optional
+ * @param {Function} fn
+ * @param {Boolean} _async
+ * @return {Model}
  */
-Model.prototype.getOption = function getOption(name, defaults) {
-  return _.result(this.constructor.options, name, defaults)
+Model.pre = function pre(name, fn, _async) {
+  this.prototype.$hooks.pre(name, fn, _async)
+  return this
 }
 
 /**
- * Return the primary key name
+ * Create and add a post callback
  * 
- * @return {String}
+ * @param {String} name
+ * @param {Function} fn
+ * @return {Model}
  */
-Model.prototype.getKeyName = function getKeyName() {
-  return this.getOption('pk')
+Model.post = function post(name, fn) {
+  this.prototype.$hooks.post(name, fn)
+  return this
 }
 
 /**
- * Identifier getter
+ * Fill the model data
+ * 
+ * @param {object} attrs
+ * @return {Model} instance
  */
-Model.prototype.getId = function getId() { 
+Model.prototype.fill = function fill(attrs) {
+  for ( var name in attrs ) { 
+    this.set(name, attrs[name])
+  }
+  
+  return this
+}
+
+/**
+ * Set the model data without any checking
+ * 
+ * @param {Object} data
+ * @param {Boolean} sync
+ * @return {Model} instance
+ */
+Model.prototype.setData = function setData(data, sync) {
+  this.$data = data
+  
+  // sync original attributes with current state
+  if ( sync === true ) this._syncOriginal()
+  
+  return this
+}
+
+/**
+ * Set model's attribute value
+ * 
+ * @param {String} attr
+ * @param {any} newVal
+ * @return {Model} instance
+ */
+Model.prototype.set = function set(attr, newVal) {
+  if ( _.isEmpty(attr) ) return this
+  
+  // TODO use attribute setter if available
+  
+  // set the new value
+  this.$data[attr] = newVal
+  
+  return this
+}
+
+/**
+ * Get the value of an attribute
+ * 
+ * @param {String} attr name
+ * @param {any} defaultValue
+ * @return {any}
+ */
+Model.prototype.get = function get(attr, defaultValue) {
+  // TODO use attribute getter if available
+  return this.$data[attr] || defaultValue
+}
+
+/**
+ * Set primary key value
+ * 
+ * @param {any} id
+ * @return {Model} instance
+ */
+Model.prototype.setId = function setId(id) {
+  return this.set(this.getKeyName(), id)
+}
+
+/**
+ * Get the primary key value
+ * 
+ * @return {any}
+ */
+Model.prototype.getId = function getId() {
   return this.get(this.getKeyName())
 }
 
 /**
- * Identifier setter
- */
-Model.prototype.setId = function setId(id) {
-  this.set(this.getKeyName(), id)
-  return this
-}
-
-/**
- * Set model attributes
- */
-Model.prototype.set = function set(key, val) {
-  if ( _.isObject(key) ) this.data.fill(key)
-  else this.data.set(key, val)
-  return this
-}
-
-/**
- * Get the value of an attribute.
+ * Get the primary key field name
  * 
- * @param {String} attr name
+ * @return {String}
  */
-Model.prototype.get = function get(attr) {
-  return this.data.get(attr)
+Model.prototype.getKeyName = function getKeyName() {
+  return "id"
 }
 
 /**
- * @param {String} attr name
+ * Get table or collection name
+ * 
+ * @return {String}
+ */
+Model.prototype.getSourceName = function getSourceName() {
+  return undefined
+}
+
+/**
+ * Determine if an attribute exists on the model
+ * 
+ * @param {String} attr
+ * @return {Boolean}
  */
 Model.prototype.has = function has(attr) {
-  return this.data.has(attr)
+  return !_.isUndefined(this.get(attr))
 }
 
 /**
+ * Return a hash of dirty fields
  * 
- */
-Model.prototype.isNew = function isNew() {
-  return !this.has(this.getKeyName())
-}
-
-/**
- * 
- */
-Model.prototype.toJSON = function toJSON() {
-  return this.data.toJSON()
-}
-
-/**
- * @param {String} attr name
- */
-Model.prototype.isDirty = function isDirty(attr) {
-  return this.data.isDirty(attr)
-}
-
-/**
- * 
+ * @return {Object}
  */
 Model.prototype.getDirty = function getDirty() {
-  return this.data.getDirty()
+  return _.pick(this.$data, function(val, attr) {
+    return !_.has(this.$original, attr) || val !== this.$original[attr]
+  }, this)
 }
 
 /**
+ * Determine if the model or given attribute have been modified
  * 
+ * @param {String} attr (optional)
+ * @return {Boolean}
  */
-Model.prototype.serialize = function serialize() {
-  return this.data.serialize()
-}
-
-/**
- * Fetch fresh data from database
- * 
- * @param {Function} callback
- */
-Model.prototype.fetch = function fetch(cb) {
-  var pk = this.getKeyName()
+Model.prototype.isDirty = function isDirty(attr) {
+  var dirty = this.getDirty()
   
-  return this.newQuery().where(pk, this.getId()).fetch(cb)
+  return attr ? _.has(dirty, attr) : !_.isEmpty(dirty)
 }
 
 /**
- * Save the model attributes
- * 
- * @param {Function} callback
+ * @return {Object}
  */
-Model.prototype.save = function save(cb) {
-  return this.isNew() ? this.insert(cb) : this.update(cb)
+Model.prototype.toJSON = function toJSON() {
+  // TODO omit hidden fields
+  return _.clone(this.$data)
 }
 
 /**
- * Destroy the model
+ * Called by the constructor to init the new instance
  * 
- * @param {Function} callback
- */
-Model.prototype.destroy = function destroy(cb) {
-  return this.newQuery().destroy(cb)
-}
-
-/**
- * 
- */
-Model.prototype.insert = function insert(cb) {
-  return this.newQuery().insert(cb)
-}
-
-/**
- * 
- */
-Model.prototype.update = function update(cb) {
-  return this.newQuery().update(cb)
-}
-
-/**
- * Create a new query for the provided model
- * 
- * @return Query builder object
- */
-Model.prototype.newQuery = function newQuery() {
-  return new Query(this, this.constructor.driver)
-}
-
-/**
- * Called by the constructor when creating a new instance
- * 
- * @param {Object} attributes
- */
-Model.prototype._init = function _init(attributes) {
-  this._initData(attributes)
-}
-
-/**
- * Setup model's data
- * 
+ * @param {Object} attrs
  * @private
  */
-Model.prototype._initData = function _initData(attributes) {
-  Object.defineProperty(this, 'data', {
-    enumerable: false,
-    value: new DataClass(this, attributes || {})
-  })
-}
-
-// HELPERS --------------------------------------------------------------------
-
-/**
- * Helper to merge options from parent class to subclasses
- */
-function mergeOptions(parent, child) {
-  var options = _.extend({}, parent)
+Model.prototype._init = function _init(attrs) {
+  this.$data = {}
+  this.$original = {}
   
-  if (! child ) return options
-  if (! parent ) return child
-  
-  // iterate over child options
-  _.each(child, function(val, key) {
-    
-    options[key] = mergeField(key, val, options[key])
-    
-  })
-  
-  return options
+  // set model attributes
+  this.fill(attrs)
 }
 
 /**
- * 
+ * Sync the original attributes with the current
  */
-function mergeField(key, newVal, oldVal) {
-  switch (key) {
-    case 'attributes':
-      return mergeAttributes(newVal, oldVal)
-      
-    case 'methods':
-    case 'statics':
-      return undefined
-    
-    default: 
-      return newVal
-  }
-}
-
-/**
- * 
- */
-function mergeAttributes(newVal, oldVal) {
-  var attrs = _.extend({}, oldVal)
-  
-  _.each(newVal, function(options, name) {
-    // normalize object format
-    if ( _.isFunction(options) ) {
-      attrs[name] = { 'type': options }
-    }
-  })
-  
-  return attrs
-}
-
-/**
- * 
- */
-function setProxies(proto, attrs) {
-  _.each(attrs, function(config, name) {
-    
-    Object.defineProperty(proto, name, {
-      enumerable: true,
-      configurable: true,
-      get: config.get || function getter() {
-        return this.get(name)
-      },
-      set: config.set || function setter(value) {
-        this.set(name, value)
-      }
-    })
-    
-  })
+Model.prototype._syncOriginal = function _syncOriginal() {
+  this.$original = _.clone(this.$data)
 }
