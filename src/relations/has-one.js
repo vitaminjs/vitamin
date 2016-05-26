1357a92e34ca1bcb2e2e8dc8ce8e650f38a02589
Abdessamad MOUHASSINE <a.mouhassine@gmail.com>
2016-05-26 12:33:53 +0200

    update hasOne relation

--- src/relations/has-one.js
+++ src/relations/has-one.js
@@ -1,31 +1,7 @@
 
-var Relation = require('./base')
+var Relation = require('./has-one-or-many')
 
 module.exports = Relation.extend({
-  
-  /**
-   * HasOne relation constructor
-   * 
-   * @param {Model} parent
-   * @param {Query} query
-   * @param {String} fk foreign key
-   * @param {String} pk local key
-   * @constructor
-   */
-  constructor: function HasOne(parent, query, fk, pk) {
-    Relation.apply(this, [parent, query])
-    this.foreignKey = fk
-    this.localKey = pk
-  },
-  
-  /**
-   * Apply relation query constraints
-   * 
-   * @param {Array} models
-   */
-  applyConstraints: function applyConstraints(models) {
-    this.query.where(this.foreignKey, this.getKeys(models, this.localKey))
-  },
   
   /**
    * Load the related model from the database
