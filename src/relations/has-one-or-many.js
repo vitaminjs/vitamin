7171a5d768a9ea41e7cc37fdfefd6d193d214fb3
Abdessamad MOUHASSINE <a.mouhassine@gmail.com>
2016-05-26 13:33:48 +0200

    add `create()` method

--- src/relations/has-one-or-many.js
+++ src/relations/has-one-or-many.js
@@ -5,13 +5,13 @@ module.exports = Relation.extend({
   
   /**
    * @param {Model} parent
-   * @param {Query} query
+   * @param {Model} related
    * @param {String} fk
    * @param {String} pk
    * @constructor
    */
-  constructor: function(parent, query, fk, pk) {
-    Relation.apply(this, [parent, query]
+  constructor: function(parent, related, fk, pk) {
+    Relation.apply(this, [parent, related]
     this.foreignKey = fk
     this.localKey = pk
   },
@@ -23,6 +23,21 @@ module.exports = Relation.extend({
    */
   applyConstraints: function applyConstraints(models) {
     this.query.where(this.foreignKey, this.getKeys(models, this.localKey))
+  },
+  
+  /**
+   * Create a new instance of the related model
+   * 
+   * @param {Object} attrs
+   * @param {Function} cb (optional)
+   * @return a promise
+   */
+  create: function create(attrs, cb) {
+    return this
+      .related
+      .newInstance(attrs)
+      .set(this.foreignKey, this.parent.get(this.localKey))
+      .save(cb)
   }
   
 })
