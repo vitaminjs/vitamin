5dbdc463c164fde17f9695353ac9bd8141832ee7
Abdessamad MOUHASSINE <a.mouhassine@gmail.com>
2016-05-26 12:39:02 +0200

    Create has-many.js

---
+++ src/relations/has-many.js
@@ -0,0 +1,16 @@
+
+var Relation = require('has-one-or-many')
+
+module.exports = Relation.extend({
+  
+  /**
+   * Load the related models from the database
+   * 
+   * @param {Function} cb (optional)
+   * @return a promise
+   */
+  _load: function _load(cb) {
+    return this.query.fetchAll(cb)
+  }
+  
+})
