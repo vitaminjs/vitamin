16bb0c4232e1fa0daddd97b14a3799a80cf6aabe
Abdessamad MOUHASSINE <a.mouhassine@gmail.com>
2016-05-27 12:46:31 +0200

    add `saveMany()` method

--- src/relations/has-many.js
+++ src/relations/has-many.js
@@ -1,7 +1,23 @@
 
-var Relation = require('has-one-or-many')
+var Promise = require('bluebird'),
+    Relation = require('has-one-or-many')
 
 module.exports = Relation.extend({
+  
+  /**
+   * Attach many models to the parent model
+   * 
+   * @param {Array} models
+   * @param {Function} cb (optional)
+   * @return Promise instance
+   */
+  saveMany: function saveMany(models, cb) {
+    return Promise
+      .bind(this)
+      .map(models, function (model) {
+        return this.save(model)
+      })
+  },
   
   /**
    * Load the related models from the database
