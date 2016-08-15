038b3561ebfb8819588d5125649544e5f1220d3d
Abdessamad MOUHASSINE <a.mouhassine@gmail.com>
2016-08-15 13:40:12 +0200

    Update registry.js

--- src/registry.js
+++ src/registry.js
@@ -1,1 +1,40 @@
 
+var models = {}
+
+// exports
+export default {
+  
+  /**
+   * Set a model in the registry
+   * 
+   * @param {String} name
+   * @param {Function} model constructor
+   * @return model
+   */
+  set(name, model) {
+    if( this.has(name) ) throw new Error(name + " is already defined in the registry")
+    
+    return models[name] = model
+  }
+  
+  /**
+   * Get the model by name
+   * 
+   * @param {String} name
+   * @return model constructor
+   */
+  get(name) {
+    return models[name]
+  }
+  
+  /**
+   * Determine if the model name exists in the registry
+   * 
+   * @param {String} name
+   * @return boolean
+   */
+  has(name) {
+    return !!this.get(name)
+  }
+  
+}
