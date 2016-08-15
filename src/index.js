b46a903492a19610fea8bac06f9db0c09b6af98d
Abdessamad MOUHASSINE <a.mouhassine@gmail.com>
2016-08-15 16:40:38 +0200

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 
 import Model from './model'
+import registry from './registry'
 
 /**
  * Initialize Vitamin
@@ -8,5 +9,20 @@ module.exports = function initialize(knex) {
   // add knex instance to all models as `connection` property
   Model.prototype.connection = knex
   
-  return { Model }
+  return {
+    
+    Model,
+    
+    /**
+     * Set/Get a model constructor
+     * 
+     * @param {String} name
+     * @param {Function} ctor
+     * @return model constructor
+     */
+    model: function(name, ctor = null) {
+      return ctor ? registry.set(...arguments) : registry.get(name)
+    }
+    
+  }
 }
