0a5ab99ad60519f51887038c6285fbafc7c9e971
Abdessamad MOUHASSINE <a.mouhassine@gmail.com>
2016-08-17 14:16:45 +0200

    Update index.js

--- src/index.js
+++ src/index.js
@@ -15,12 +15,18 @@ module.exports = function initialize(knex) {
      * Set/Get a model constructor
      * 
      * @param {String} name
-     * @param {Object} options
+     * @param {Object} options object or a mapper instance
      * @return model constructor
      */
     model: function(name, options = null) {
-      if ( options ) {
-        var mapper = options instanceof Mapper ? options : new Mapper(options)
+      if ( options != null ) {
+        let mapper
+        
+        if ( options instanceof Mapper ) mapper = options
+        else {
+          options.name = name
+          mapper = new Mapper(options)
+        }
         
         return registry.set(name, mapper.use(knex).build())
       }
